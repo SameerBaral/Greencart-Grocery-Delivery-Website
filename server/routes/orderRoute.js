c5893cbecb96925a8e294dd8107167771086563f
Sameer Baral <baralsameer70@gmail.com>
2026-09-24 21:09:57 +0530

    Route address and order endpoints directly to self-authenticating controllers

--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -1,14 +1,13 @@
 import express from 'express';
-import authUser from '../middlewares/authUser.js';
 import { getAllOrders, getUserOrders, placeOrderCOD, placeOrderStripe, verifyStripe } from '../controllers/orderController.js';
 import authSeller from '../middlewares/authSeller.js';
 
 const orderRouter = express.Router();
 
-orderRouter.post('/cod', authUser, placeOrderCOD)
-orderRouter.get('/user', authUser, getUserOrders)
+orderRouter.post('/cod', placeOrderCOD)
+orderRouter.get('/user', getUserOrders)
 orderRouter.get('/seller', authSeller, getAllOrders)
-orderRouter.post('/stripe', authUser, placeOrderStripe)
-orderRouter.post('/verifyStripe', authUser, verifyStripe)
+orderRouter.post('/stripe', placeOrderStripe)
+orderRouter.post('/verifyStripe', verifyStripe)
 
 export default orderRouter;
